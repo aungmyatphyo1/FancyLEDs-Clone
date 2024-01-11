8461be65425107b8349956c1505d4485f6af3073
aungmyatphyo <aungmyat92.11@gmail.com>
2024-01-11 21:25:11 +0630

    FancyLEDs Website Clone

---
+++ main.js
@@ -0,0 +1,10 @@
+// const csImg = document.querySelectorAll("#csImg");
+const csImg = document.querySelector("#csImg");
+const toggleImg1 = () => {
+  csImg.classList.toggle("z-20");
+};
+
+const seImg = document.querySelector("#seImg");
+const toggleImg2 = () => {
+  seImg.classList.toggle("z-20");
+};
